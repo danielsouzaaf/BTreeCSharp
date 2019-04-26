b60e22234b221b22ca80a3b72028eaad46a5969a
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-04-26 13:57:50 -0300

    Classe nó e entrada

---
+++ BTree/BTree/Entrada.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BTree
+{
+
+    public class Entrada<TC, TV> : IEquatable<Entrada<TC, TV>>
+    {
+        public TC Chave { get; set; }
+
+        public TV Valor { get; set; }
+
+        public bool Equals(Entrada<TC, TV> outro)
+        {
+            return this.Chave.Equals(outro.Chave) && this.Valor.Equals(outro.Valor);
+        }
+
+
+    }
+}
---
+++ BTree/BTree/No.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTree
+{
+    class No<TK, TC>
+    {
+        private int t;
+
+        public List<No<TK, TC>> Filhos { get; set; }
+
+        public List<Entrada<TK, TC>> Entradas { get; set; }
+
+        public No(int t)
+        {
+            this.t = t;
+            this.Filhos = new List<No<TK, TC>>(t);
+            this.Entradas = new List<Entrada<TK, TC>>(t);
+        }
+
+        public Boolean ehFolha
+        {
+            get
+            {
+                return this.Filhos.Count == 0;
+            }
+        }
+
+        public bool atingiuMaximoEntradas
+        {
+            get
+            {
+                return this.Entradas.Count == (2 * this.t) - 1;
+            }
+        }
+
+        public bool atingiuMinimoEntradas
+        {
+            get
+            {
+                return this.Entradas.Count == this.t - 1;
+            }
+        }
+    }
+}
